a8a7e250fa11654a74dbca49083dd7e8ad7078fe
HeBilLi <50161529+HeBilLi@users.noreply.github.com>
2021-05-24 13:37:15 +0800

    styling

--- README.md
+++ README.md
@@ -10,15 +10,15 @@ Answer: 1
 
 2. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
 
-Answer: No. Unit testing is more focus on the individual parts of the bigger project. The "message" feature of a messaging application is the overall goal and involves many smaller features.
+Answer: We should not to use the unit test to test the "message" function of a messaging application. Unit testing is more focus on the individual parts of the bigger project. The "message" feature of a messaging application is the overall goal and involves many smaller features.
 
 3. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters
 
-Answer: No. The unit test is not for the load testing. Unit testing should focus more on the feature and funtionality of smaller parts of code.
+Answer: I think we can use unit tests to test the max message length feature. The max message length feature is one part of features of the overall messaging functionality. Therefore it is resonable to test this small feature using unit testing
 
 4. What do you expect to happen if we run our puppeteer tests with the field “headless” set to true?
 
-Answer: If we set the headless to be true,Puppeteer will not be able to drive the browser to simulate the user interaction. 
+Answer: If we set the headless to be true, Puppeteer will not open the browser window.
 
 
 5. What would your beforeAll callback look like if you wanted to start from the settings page before every test case?
--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -1,36 +1,41 @@
-describe('Basic user flow for SPA ', () => {
+describe("Basic user flow for SPA ", () => {
   beforeAll(async () => {
-    await page.goto('http://127.0.0.1:5500');
+    await page.goto("http://127.0.0.1:5500");
     await page.waitForTimeout(500);
-
   });
 
   // test 1 is given
-  it('Test1: Initial Home Page - Check for 10 Journal Entries', async () => {
-    const numEntries = await page.$$eval('journal-entry', (entries) => {
+  it("Test1: Initial Home Page - Check for 10 Journal Entries", async () => {
+    const numEntries = await page.$$eval("journal-entry", (entries) => {
       return entries.length;
     });
     expect(numEntries).toBe(10);
   });
 
   // test 2 is given
-  it('Test2: Make sure <journal-entry> elements are populated', async () => {
+  it("Test2: Make sure <journal-entry> elements are populated", async () => {
     let allArePopulated = true;
     let data, plainValue;
-    const entries = await page.$$('journal-entry');
+    const entries = await page.$$("journal-entry");
     for (let i = 0; i < entries.length; i++) {
-      data = await entries[i].getProperty('entry');
+      data = await entries[i].getProperty("entry");
       plainValue = await data.jsonValue();
-      if (plainValue.title.length == 0) { allArePopulated = false; }
-      if (plainValue.date.length == 0) { allArePopulated = false; }
-      if (plainValue.content.length == 0) { allArePopulated = false; }
+      if (plainValue.title.length == 0) {
+        allArePopulated = false;
+      }
+      if (plainValue.date.length == 0) {
+        allArePopulated = false;
+      }
+      if (plainValue.content.length == 0) {
+        allArePopulated = false;
+      }
     }
     expect(allArePopulated).toBe(true);
   }, 30000);
 
-  it('Test3: Clicking first <journal-entry>, new URL should contain /#entry1', async () => {
+  it("Test3: Clicking first <journal-entry>, new URL should contain /#entry1", async () => {
     // implement test3: Clicking on the first journal entry should update the URL to contain “/#entry1”
-    const entries = await page.$$('journal-entry');
+    const entries = await page.$$("journal-entry");
     await entries[0].click();
 
     await page.waitForNavigation();
@@ -39,65 +44,67 @@ describe('Basic user flow for SPA ', () => {
     expect(eveal).toBe(true);
   });
 
-  it('Test4: On first Entry page - checking page header title', async () => {
-    // implement test4: Clicking on the first journal entry should update the header text to “Entry 1” 
-    const title = await page.$eval('h1', (title) => {
+  it("Test4: On first Entry page - checking page header title", async () => {
+    // implement test4: Clicking on the first journal entry should update the header text to “Entry 1”
+    const title = await page.$eval("h1", (title) => {
       return title.textContent;
     });
     expect(title == "Entry 1").toBe(true);
-
   });
 
-  it('Test5: On first Entry page - checking <entry-page> contents', async () => {
+  it("Test5: On first Entry page - checking <entry-page> contents", async () => {
     let check = true;
 
-    const title = await page.$eval('entry-page', (page) => {
+    const title = await page.$eval("entry-page", (page) => {
       return page.entry.title;
     });
     if (title != "You like jazz?") {
       check = false;
     }
 
-    const date = await page.$eval('entry-page', (page) => {
+    const date = await page.$eval("entry-page", (page) => {
       return page.entry.date;
     });
     if (date != "4/25/2021") {
       check = false;
     }
 
-    const content = await page.$eval('entry-page', (page) => {
+    const content = await page.$eval("entry-page", (page) => {
       return page.entry.content;
     });
-    if (content != "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.") {
+    if (
+      content !=
+      "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible."
+    ) {
       check = false;
     }
 
-
-    const img = await page.$eval('entry-page', (page) => {
+    const img = await page.$eval("entry-page", (page) => {
       return page.entry.image;
     });
-    if (img.src != 'https://i1.wp.com/www.thepopcornmuncher.com/wp-content/uploads/2016/11/bee-movie.jpg?resize=800%2C455') {
+    if (
+      img.src !=
+      "https://i1.wp.com/www.thepopcornmuncher.com/wp-content/uploads/2016/11/bee-movie.jpg?resize=800%2C455"
+    ) {
       check = false;
     }
-    if (img.alt != 'bee with sunglasses') {
+    if (img.alt != "bee with sunglasses") {
       check = false;
     }
-
 
     expect(check).toBe(true);
-
   }, 10000);
 
-  it('Test6: On first Entry page - checking <body> element classes', async () => {
+  it("Test6: On first Entry page - checking <body> element classes", async () => {
     // implement test6: Clicking on the first journal entry should update the class attribute of <body> to ‘single-entry’
-    const className = await page.$eval('body', (page) => {
+    const className = await page.$eval("body", (page) => {
       return page.className;
     });
 
     expect(className).toBe("single-entry");
   });
 
-  it('Test7: Clicking the settings icon, new URL should contain #settings', async () => {
+  it("Test7: Clicking the settings icon, new URL should contain #settings", async () => {
     // implement test7: Clicking on the settings icon should update the URL to contain “/#settings”
 
     const setting = await page.$('img[alt="settings"]');
@@ -106,30 +113,26 @@ describe('Basic user flow for SPA ', () => {
     const url = page.url();
     let eveal = url.indexOf("/#settings") > -1;
     expect(eveal).toBe(true);
-
   });
 
-  it('Test8: On Settings page - checking page header title', async () => {
+  it("Test8: On Settings page - checking page header title", async () => {
     // implement test8: Clicking on the settings icon should update the header to be “Settings”
-    const title = await page.$eval('h1', (title) => {
+    const title = await page.$eval("h1", (title) => {
       return title.textContent;
     });
     expect(title == "Settings").toBe(true);
-
-
   });
 
-  it('Test9: On Settings page - checking <body> element classes', async () => {
+  it("Test9: On Settings page - checking <body> element classes", async () => {
     // implement test9: Clicking on the settings icon should update the class attribute of <body> to ‘settings’
-    const className = await page.$eval('body', (page) => {
+    const className = await page.$eval("body", (page) => {
       return page.className;
     });
 
     expect(className).toBe("settings");
-
   });
 
-  it('Test10: Clicking the back button, new URL should be /#entry1', async () => {
+  it("Test10: Clicking the back button, new URL should be /#entry1", async () => {
     // implement test10: Clicking on the back button should update the URL to contain ‘/#entry1’
     await page.goBack();
 
@@ -139,38 +142,34 @@ describe('Basic user flow for SPA ', () => {
   });
 
   // define and implement test11: Clicking the back button once should bring the user back to the home page
-  it('Test11: Clicking the back button, new URL should be http://127.0.0.1:5500/', async () => {
-
+  it("Test11: Clicking the back button, new URL should be http://127.0.0.1:5500/", async () => {
     await page.goBack();
 
     const url = page.url();
-    let eveal = url == 'http://127.0.0.1:5500/';
+    let eveal = url == "http://127.0.0.1:5500/";
     expect(eveal).toBe(true);
   });
 
   // define and implement test12: When the user if on the homepage, the header title should be “Journal Entries”
-  it('Test12: When the user if on the homepage, the header title should be “Journal Entries', async () => {
-
-    const title = await page.$eval('h1', (title) => {
+  it("Test12: When the user if on the homepage, the header title should be “Journal Entries", async () => {
+    const title = await page.$eval("h1", (title) => {
       return title.textContent;
     });
     expect(title == "Journal Entries").toBe(true);
   });
 
-  // define and implement test13: On the home page the <body> element should not have any class attribute 
-  it('Test13: On the home page the <body> element should not have any class attribute', async () => {
-
-    const classLength = await page.$eval('body', (page) => {
+  // define and implement test13: On the home page the <body> element should not have any class attribute
+  it("Test13: On the home page the <body> element should not have any class attribute", async () => {
+    const classLength = await page.$eval("body", (page) => {
       return page.classList.length;
     });
 
     expect(classLength).toBe(0);
-
   });
 
   // define and implement test14: Verify the url is correct when clicking on the second entry
-  it('Test14: Clicking second <journal-entry>, new URL should contain /#entry2', async () => {
-    const entries = await page.$$('journal-entry');
+  it("Test14: Clicking second <journal-entry>, new URL should contain /#entry2", async () => {
+    const entries = await page.$$("journal-entry");
     await entries[1].click();
 
     await page.waitForNavigation();
@@ -180,71 +179,69 @@ describe('Basic user flow for SPA ', () => {
   });
 
   // define and implement test15: Verify the title is current when clicking on the second entry
-  it('Test15: On second Entry page - checking page header title', async () => {
-    const title = await page.$eval('h1', (title) => {
+  it("Test15: On second Entry page - checking page header title", async () => {
+    const title = await page.$eval("h1", (title) => {
       return title.textContent;
     });
     expect(title == "Entry 2").toBe(true);
-
   });
 
-
   // define and implement test16: Verify the entry page contents is correct when clicking on the second entry
-  it('Test16: On second Entry page - checking <entry-page> contents', async () => {
+  it("Test16: On second Entry page - checking <entry-page> contents", async () => {
     let check = true;
 
-    const title = await page.$eval('entry-page', (page) => {
+    const title = await page.$eval("entry-page", (page) => {
       return page.entry.title;
     });
     if (title != "Run, Forrest! Run!") {
       check = false;
     }
 
-    const date = await page.$eval('entry-page', (page) => {
+    const date = await page.$eval("entry-page", (page) => {
       return page.entry.date;
     });
     if (date != "4/26/2021") {
       check = false;
     }
 
-    const content = await page.$eval('entry-page', (page) => {
+    const content = await page.$eval("entry-page", (page) => {
       return page.entry.content;
     });
-    if (content != "Mama always said life was like a box of chocolates. You never know what you're gonna get.") {
+    if (
+      content !=
+      "Mama always said life was like a box of chocolates. You never know what you're gonna get."
+    ) {
       check = false;
     }
 
-
-    const img = await page.$eval('entry-page', (page) => {
+    const img = await page.$eval("entry-page", (page) => {
       return page.entry.image;
     });
-    if (img.src != 'https://s.abcnews.com/images/Entertainment/HT_forrest_gump_ml_140219_4x3_992.jpg') {
+    if (
+      img.src !=
+      "https://s.abcnews.com/images/Entertainment/HT_forrest_gump_ml_140219_4x3_992.jpg"
+    ) {
       check = false;
     }
-    if (img.alt != 'forrest running') {
+    if (img.alt != "forrest running") {
       check = false;
     }
-
 
     expect(check).toBe(true);
-
   }, 10000);
 
   // define and implement test17: Clicking the back button once should bring the user back to the home page
-  it('Test17: Clicking the back button, new URL should be http://127.0.0.1:5500/', async () => {
-
+  it("Test17: Clicking the back button, new URL should be http://127.0.0.1:5500/", async () => {
     await page.goBack();
 
     const url = page.url();
-    let eveal = url == 'http://127.0.0.1:5500/';
+    let eveal = url == "http://127.0.0.1:5500/";
     expect(eveal).toBe(true);
   });
 
-
-
   // define and implement test18: Verify the url is correct when clicking on the third entry
-  it('Test18: Clicking third <journal-entry>, new URL should contain /#entry3', async () => {
-    const entries = await page.$$('journal-entry');
+  it("Test18: Clicking third <journal-entry>, new URL should contain /#entry3", async () => {
+    const entries = await page.$$("journal-entry");
     await entries[2].click();
 
     await page.waitForNavigation();
@@ -255,68 +252,72 @@ describe('Basic user flow for SPA ', () => {
   });
 
   // define and implement test19: Verify the title is correct when clicking on the third entry
-  it('Test19: On third Entry page - checking page header title', async () => {
-    const title = await page.$eval('h1', (title) => {
+  it("Test19: On third Entry page - checking page header title", async () => {
+    const title = await page.$eval("h1", (title) => {
       return title.textContent;
     });
     expect(title == "Entry 3").toBe(true);
-
   });
 
   // define and implement test20: Verify the entry page contents is correct when clicking on the third entry
-  it('Test20: On second Entry page - checking <entry-page> contents', async () => {
+  it("Test20: On second Entry page - checking <entry-page> contents", async () => {
     let check = true;
 
-    const title = await page.$eval('entry-page', (page) => {
+    const title = await page.$eval("entry-page", (page) => {
       return page.entry.title;
     });
     if (title != "Ogres are like onions") {
       check = false;
     }
 
-    const date = await page.$eval('entry-page', (page) => {
+    const date = await page.$eval("entry-page", (page) => {
       return page.entry.date;
     });
     if (date != "4/27/2021") {
       check = false;
     }
 
-    const content = await page.$eval('entry-page', (page) => {
+    const content = await page.$eval("entry-page", (page) => {
       return page.entry.content;
     });
-    if (content != "Onions have layers. Ogres have layers. Onions have layers. You get it? We both have layers.") {
+    if (
+      content !=
+      "Onions have layers. Ogres have layers. Onions have layers. You get it? We both have layers."
+    ) {
       check = false;
     }
 
-
-    const img = await page.$eval('entry-page', (page) => {
+    const img = await page.$eval("entry-page", (page) => {
       return page.entry.image;
     });
-    if (img.src != 'https://advancelocal-adapter-image-uploads.s3.amazonaws.com/image.syracuse.com/home/syr-media/width2048/img/entertainment_impact/photo/shrek-donkeyjpg-daa31aa2b5bedfaa.jpg') {
+    if (
+      img.src !=
+      "https://advancelocal-adapter-image-uploads.s3.amazonaws.com/image.syracuse.com/home/syr-media/width2048/img/entertainment_impact/photo/shrek-donkeyjpg-daa31aa2b5bedfaa.jpg"
+    ) {
       check = false;
     }
-    if (img.alt != 'shrek and donkey looking confused') {
+    if (img.alt != "shrek and donkey looking confused") {
       check = false;
     }
-
 
     expect(check).toBe(true);
-
   }, 10000);
 
-  // define and implement test21: Clicking the back button once should bring the user back to the home page
-  it('Test21: Clicking the back button, new URL should be http://127.0.0.1:5500/', async () => {
+  //Note: I included more than 20 tests because there are several tests checking the same thing (going back to main page)
+  //I checked with Cora and she said it is fine
 
+  // define and implement test21: Clicking the back button once should bring the user back to the home page
+  it("Test21: Clicking the back button, new URL should be http://127.0.0.1:5500/", async () => {
     await page.goBack();
 
     const url = page.url();
-    let eveal = url == 'http://127.0.0.1:5500/';
+    let eveal = url == "http://127.0.0.1:5500/";
     expect(eveal).toBe(true);
   });
 
   // define and implement test22: Verify the url is correct when clicking on the fourth entry
-  it('Test22: Clicking third <journal-entry>, new URL should contain /#entry4', async () => {
-    const entries = await page.$$('journal-entry');
+  it("Test22: Clicking third <journal-entry>, new URL should contain /#entry4", async () => {
+    const entries = await page.$$("journal-entry");
     await entries[3].click();
 
     await page.waitForNavigation();
@@ -324,5 +325,4 @@ describe('Basic user flow for SPA ', () => {
     let eveal = url.indexOf("/#entry4") > -1;
     expect(eveal).toBe(true);
   });
-
 });
